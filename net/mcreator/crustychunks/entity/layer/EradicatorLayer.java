20fa76f63250da642a100606f90f07c70f28c230
recyclingofideastypa <luke.singleton.234@gmail.com>
2025-08-27 05:08:40 -0700

    Add files via upload

---
+++ net/mcreator/crustychunks/entity/layer/EradicatorLayer.java
@@ -0,0 +1,25 @@
+package net.mcreator.crustychunks.entity.layer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.mcreator.crustychunks.entity.EradicatorEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.renderer.GeoRenderer;
+import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
+
+public class EradicatorLayer extends GeoRenderLayer<EradicatorEntity> {
+   private static final ResourceLocation LAYER = new ResourceLocation("crusty_chunks", "textures/entities/eradicatorglow.png");
+
+   public EradicatorLayer(GeoRenderer<EradicatorEntity> entityRenderer) {
+      super(entityRenderer);
+   }
+
+   public void render(PoseStack poseStack, EradicatorEntity animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+      RenderType glowRenderType = RenderType.m_110488_(LAYER);
+      this.getRenderer().reRender(this.getDefaultBakedModel(animatable), poseStack, bufferSource, animatable, glowRenderType, bufferSource.m_6299_(glowRenderType), partialTick, packedLight, OverlayTexture.f_118083_, 1.0F, 1.0F, 1.0F, 1.0F);
+   }
+}
