cb56d2f4de8cbe1ae57988cf33891aceaeed3cc9
recyclingofideastypa <luke.singleton.234@gmail.com>
2025-08-27 05:01:57 -0700

    Add files via upload

---
+++ net/mcreator/crustychunks/client/screens/ScopeOverlay2Overlay.java
@@ -0,0 +1,54 @@
+package net.mcreator.crustychunks.client.screens;
+
+import com.mojang.blaze3d.platform.GlStateManager.DestFactor;
+import com.mojang.blaze3d.platform.GlStateManager.SourceFactor;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.mcreator.crustychunks.procedures.ScopeOverlay2ConditionProcedure;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGuiEvent.Pre;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber({Dist.CLIENT})
+public class ScopeOverlay2Overlay {
+   @SubscribeEvent(
+      priority = EventPriority.NORMAL
+   )
+   public static void eventHandler(Pre event) {
+      int w = event.getWindow().m_85445_();
+      int h = event.getWindow().m_85446_();
+      Level world = null;
+      double x = 0.0D;
+      double y = 0.0D;
+      double z = 0.0D;
+      Player entity = Minecraft.m_91087_().f_91074_;
+      if (entity != null) {
+         world = entity.m_9236_();
+         x = entity.m_20185_();
+         y = entity.m_20186_();
+         z = entity.m_20189_();
+      }
+
+      RenderSystem.disableDepthTest();
+      RenderSystem.depthMask(false);
+      RenderSystem.enableBlend();
+      RenderSystem.setShader(GameRenderer::m_172817_);
+      RenderSystem.blendFuncSeparate(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA, SourceFactor.ONE, DestFactor.ZERO);
+      RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+      if (ScopeOverlay2ConditionProcedure.execute(entity)) {
+         event.getGuiGraphics().m_280163_(new ResourceLocation("crusty_chunks:textures/screens/lightscope.png"), 0, 0, 0.0F, 0.0F, w, h, w, h);
+      }
+
+      RenderSystem.depthMask(true);
+      RenderSystem.defaultBlendFunc();
+      RenderSystem.enableDepthTest();
+      RenderSystem.disableBlend();
+      RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+   }
+}
